dcbdcfe1a9981a2284aca1b2bfddad42de2935b9
krisdobal <39825321+krisdobal@users.noreply.github.com>
2018-06-15 10:58:29 +0200

    lcd char optimized

--- inc/lcd.h
+++ inc/lcd.h
@@ -3,5 +3,6 @@
 #define _LCD_H_
 
 void lcdRenderGame(ball_t * balls, uint8_t * activeBalls, uint32_t * striker0, uint32_t * striker1, uint32_t * bricks, uint8_t * buffer);
+void lcdCleanScreen(uint8_t * buffer_p);
 
 #endif /* _LCD_H_ */
