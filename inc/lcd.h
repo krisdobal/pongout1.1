5272b5f3d15e7b2057c106209705e9314a240b9b
FlipTheEgg <38611657+FlipTheEgg@users.noreply.github.com>
2018-06-15 15:46:48 +0200

    Added lcdRenderString

--- inc/lcd.h
+++ inc/lcd.h
@@ -2,6 +2,7 @@
 #ifndef _LCD_H_
 #define _LCD_H_
 
+void lcdRenderString(int slice, uint8_t line, uint8_t * string_p, uint8_t * buffer_p);
 void lcdRenderGame(ball_t * balls, uint8_t * activeBalls, uint32_t * striker0, uint32_t * striker1, uint32_t * bricks, uint8_t * buffer);
 void lcdCleanScreen(uint8_t * buffer_p);
 
